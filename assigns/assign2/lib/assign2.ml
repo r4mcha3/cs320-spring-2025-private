cade247845ab168cbe6aa7f7b79449e6715aa083
r4mcha3 <rchae@bu.edu>
2025-02-06 18:59:20 -0500

    Update assign2.ml

--- assigns/assign2/lib/assign2.ml
+++ assigns/assign2/lib/assign2.ml
@@ -1,42 +1,17 @@
 open Stdlib320
 
-type int_list_or_string_list = 
+type int_list_or_string_list =
   | IntList of int list
   | StringList of string list
 
- type int_or_string = 
+type int_or_string =
   | Int of int
   | String of string
-
-let convert (lst : int_or_string list) : int_list_or_string_list list =
-  let rec aux acc current = function
-    | [] -> (match current with None -> acc | Some group -> group :: acc)
-    | Int x :: rest ->
-      (match current with
-       | Some (IntList ints) -> aux acc (Some (IntList (x :: ints))) rest
-       | Some (StringList strs) -> aux (StringList (List.rev strs) :: acc) (Some (IntList [x])) rest
-       | None -> aux acc (Some (IntList [x])) rest)
-    | String s :: rest ->
-      (match current with
-       | Some (StringList strs) -> aux acc (Some (StringList (s :: strs))) rest
-       | Some (IntList ints) -> aux (IntList (List.rev ints) :: acc) (Some (StringList [s])) rest
-       | None -> aux acc (Some (StringList [s])) rest)
-  in
-  List.rev (aux [] None lst)
 
 type recipe = {
   name : string;
   ingrs : string list;
 }
-
-let rec all f lst =
-  match lst with
-  | [] -> true
-  | x :: xs -> f x && all f xs
-
-let recipes_by_ingrs (recs : recipe list) (ingrs : string list) : recipe list =
-  let contains_all ings1 ings2 = all (fun i -> List.mem i ings2) ings1 in
-  List.filter (fun r -> contains_all r.ingrs ingrs) recs
 
 type mem_status = 
   | Free
@@ -52,41 +27,58 @@ type free_result =
   | Success of memory
   | Invalid_position
 
-let rec rev_append lst acc =
-  match lst with
-  | [] -> acc
-  | x :: xs -> rev_append xs (x :: acc)
+let convert (lst : int_or_string list) : int_list_or_string_list list =
+  let rec aux acc current = function
+    | [] -> (match current with None -> acc | Some v -> v :: acc)
+    | Int x :: xs -> (match current with
+        | Some (IntList ints) -> aux acc (Some (IntList (x :: ints))) xs
+        | Some (StringList strs) -> aux (StringList (List.rev strs) :: acc) (Some (IntList [x])) xs
+        | None -> aux acc (Some (IntList [x])) xs)
+    | String s :: xs -> (match current with
+        | Some (StringList strs) -> aux acc (Some (StringList (s :: strs))) xs
+        | Some (IntList ints) -> aux (IntList (List.rev ints) :: acc) (Some (StringList [s])) xs
+        | None -> aux acc (Some (StringList [s])) xs)
+  in
+  List.rev (aux [] None lst)
+
+let recipes_by_ingrs (recs : recipe list) (ingrs : string list) : recipe list =
+  let rec subset lst1 lst2 =
+    match lst1 with
+    | [] -> true
+    | x :: xs -> List.mem x lst2 && subset xs lst2
+  in
+  let rec filter_recipes acc = function
+    | [] -> List.rev acc
+    | r :: rs ->
+        if subset r.ingrs ingrs then filter_recipes (r :: acc) rs
+        else filter_recipes acc rs
+  in
+  filter_recipes [] recs
 
 let allocate (size : int) (mem : memory) : alloc_result =
-  if size <= 0 then Invalid_size
-  else
-    let rec find_fit pos acc = function
-      | [] -> Invalid_size
-      | (Free, fsize) :: rest when fsize >= size ->
-        let occupied_chunk = (Occupied, size) in
-        let remaining_free = fsize - size in
-        let updated_mem =
-          if remaining_free > 0 then (occupied_chunk :: (Free, remaining_free) :: rest)
-          else (occupied_chunk :: rest)
-        in
-        Success (pos, rev_append acc updated_mem)
-      | chunk :: rest -> find_fit (pos + snd chunk) (chunk :: acc) rest
-    in
-    find_fit 0 [] mem
+  if size <= 0 then Invalid_size else
+  let rec find_slot pos acc = function
+    | [] -> Invalid_size
+    | (Free, n) :: rest when n >= size ->
+        let updated = if n = size then (Occupied, size) :: rest
+                      else (Occupied, size) :: (Free, n - size) :: rest in
+        Success (pos, List.rev_append acc updated)
+    | chunk :: rest -> find_slot (pos + snd chunk) (chunk :: acc) rest
+  in
+  find_slot 0 [] mem
 
 let free (pos : int) (mem : memory) : free_result =
-  if pos < 0 then Invalid_position
-  else
-    let rec free_chunk acc current_pos = function
-      | [] -> Invalid_position
-      | (Occupied, size) :: rest when current_pos = pos ->
-        let new_mem = rev_append acc ((Free, size) :: rest) in
-        let rec merge_free = function
-          | (Free, s1) :: (Free, s2) :: tl -> merge_free ((Free, s1 + s2) :: tl)
-          | hd :: tl -> hd :: merge_free tl
+  let rec free_helper acc current_pos = function
+    | [] -> Invalid_position
+    | (Occupied, n) :: rest when current_pos = pos ->
+        let new_mem = (Free, n) :: rest in
+        let rec merge mem =
+          match mem with
+          | (Free, n1) :: (Free, n2) :: rest -> merge ((Free, n1 + n2) :: rest)
+          | chunk :: rest -> chunk :: merge rest
           | [] -> []
         in
-        Success (merge_free new_mem)
-      | chunk :: rest -> free_chunk (chunk :: acc) (current_pos + snd chunk) rest
-    in
-    free_chunk [] 0 mem    
+        Success (List.rev_append acc (merge new_mem))
+    | chunk :: rest -> free_helper (chunk :: acc) (current_pos + snd chunk) rest
+  in
+  if pos < 0 then Invalid_position else free_helper [] 0 mem
