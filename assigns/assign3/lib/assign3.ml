f201798428f054cd5f9c8591ad1bbed9257d960f
r4mcha3 <rchae@bu.edu>
2025-02-14 15:20:21 -0500

    Update assign3.ml

--- assigns/assign3/lib/assign3.ml
+++ assigns/assign3/lib/assign3.ml
@@ -14,7 +14,7 @@ let rec ntree_of_tree (t : 'a tree) : 'a Stdlib320.ntree option =
         (match left_ntree with Some nt -> [nt] | None -> []) @
         (match right_ntree with Some nt -> [nt] | None -> [])
       in
-      Some (Ntree.Node (x, children))
+      Some (Stdlib320.Ntree.create x children)
 
 let fib3_tail (a, b, c) n =
   let rec aux i x y z =
@@ -26,38 +26,38 @@ let fib3_tail (a, b, c) n =
   | 1 -> b
   | 2 -> c
   | _ -> aux 3 a b c
-  
-  let file_tree root paths =
-    let rec insert_path path_parts children =
-      match path_parts with
-      | [] -> children
-      | part :: rest ->
-          let child =
-            match List.find_opt (fun (Ntree.Node (name, _)) -> name = part) children with
-            | Some (Ntree.Node (_, subchildren)) ->
-                Ntree.Node (part, insert_path rest subchildren)
-            | None ->
-                Ntree.Node (part, insert_path rest [])
-          in
-          child :: List.filter (fun (Ntree.Node (name, _)) -> name <> part) children
-    in
-    let root_children =
-      List.fold_left (fun acc path ->
-        let parts = String.split_on_char '/' path in
-        insert_path parts acc
-      ) [] paths
-    in
-    Ntree.Node (root, root_children)
-    
-  type expr =
+
+let file_tree root paths =
+  let rec insert_path path_parts children =
+    match path_parts with
+    | [] -> children
+    | part :: rest ->
+        let child =
+          match List.find_opt (fun (Stdlib320.Ntree.Node (name, _)) -> name = part) children with
+          | Some (Stdlib320.Ntree.Node (_, subchildren)) ->
+              Stdlib320.Ntree.create part (insert_path rest subchildren)
+          | None ->
+              Stdlib320.Ntree.create part (insert_path rest [])
+        in
+        child :: List.filter (fun (Stdlib320.Ntree.Node (name, _)) -> name <> part) children
+  in
+  let root_children =
+    List.fold_left (fun acc path ->
+      let parts = String.split_on_char '/' path in
+      insert_path parts acc
+    ) [] paths
+  in
+  Stdlib320.Ntree.create root root_children
+
+type expr =
   | Num of int
   | Var of string
   | Add of expr * expr
   | Mul of expr * expr
-  
-  let rec subst e1 x e2 =
-    match e2 with
-    | Num _ -> e2
-    | Var y -> if y = x then e1 else e2
-    | Add (left, right) -> Add (subst e1 x left, subst e1 x right)
-    | Mul (left, right) -> Mul (subst e1 x left, subst e1 x right)
+
+let rec subst e1 x e2 =
+  match e2 with
+  | Num _ -> e2
+  | Var y -> if y = x then e1 else e2
+  | Add (left, right) -> Add (subst e1 x left, subst e1 x right)
+  | Mul (left, right) -> Mul (subst e1 x left, subst e1 x right)
