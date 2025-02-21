9b18954475486e6c2072323d153f26580e091948
r4mcha3 <rchae@bu.edu>
2025-02-20 19:51:24 -0500

    Update assign4.ml

--- assigns/assign4/lib/assign4.ml
+++ assigns/assign4/lib/assign4.ml
@@ -65,31 +65,33 @@ type distr = (int * rat) list
 
 let rec random_walk (walk : int -> int list) (start : int) (num_steps : int) : distr =
   if num_steps = 0 then
-    [(start, { num = 1; denom = 1 })]  (* Base case: 100% probability at start *)
+    [(start, { num = 1; denom = 1 })] 
   else
     let prev_distr = random_walk walk start (num_steps - 1) in
-    let combine_prob (distr : distr) : distr =
-      let tbl = Hashtbl.create 10 in
-      List.iter
-        (fun (v, { num; denom }) ->
-           let neighbors = walk v in
-           let num_neighbors = List.length neighbors in
-           let new_prob = { num; denom = denom * num_neighbors } in
-           List.iter
-             (fun n ->
-                Hashtbl.replace tbl n
-                  (match Hashtbl.find_opt tbl n with
-                   | Some { num = num'; denom = denom' } ->
-                       let common_denom = denom' * new_prob.denom in
-                       let updated_num =
-                         (num' * new_prob.denom) + (new_prob.num * denom')
-                       in
-                       { num = updated_num; denom = common_denom }
-                   | None -> new_prob))
-             neighbors)
-        distr;
-      (* Convert hashtable back to sorted list and reduce fractions *)
-      Hashtbl.fold (fun k v acc -> (k, v) :: acc) tbl []
-      |> List.sort (fun (a, _) (b, _) -> compare a b)
+
+    let rec merge_prob v prob acc =
+      match acc with
+      | [] -> [(v, prob)]
+      | (w, p) :: rest ->
+          if v = w then
+            let common_denom = p.denom * prob.denom in
+            let new_num = (p.num * prob.denom) + (prob.num * p.denom) in
+            (w, { num = new_num; denom = common_denom }) :: rest
+          else
+            (w, p) :: merge_prob v prob rest
     in
-    combine_prob prev_distr
+    let rec process_distribution distr acc =
+      match distr with
+      | [] -> acc
+      | (v, { num; denom }) :: rest ->
+          let neighbors = walk v in
+          let num_neighbors = List.length neighbors in
+          let new_prob = { num; denom = denom * num_neighbors } in
+          let updated_acc =
+            List.fold_left (fun acc n -> merge_prob n new_prob acc) acc neighbors
+          in
+          process_distribution rest updated_acc
+    in
+
+    let final_distr = process_distribution prev_distr [] in
+    List.sort (fun (a, _) (b, _) -> compare a b) final_distr
