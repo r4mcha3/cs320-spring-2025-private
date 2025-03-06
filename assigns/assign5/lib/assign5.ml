5337e1434bcd70e7d1a817c7e0d7814472befab0
r4mcha3 <rchae@bu.edu>
2025-03-06 17:46:18 -0500

    Update assign5.ml

--- assigns/assign5/lib/assign5.ml
+++ assigns/assign5/lib/assign5.ml
@@ -28,20 +28,29 @@ let eval (e : 'a expr) : (int, 'a error) result =
     match e.expr with
     | Num n -> Ok n
     | Op (op, e1, e2) ->
-      let* v1 = eval_expr e1 in  (* First evaluate e1 *)
-      let* v2 = eval_expr e2 in  (* Only evaluate e2 if e1 succeeded *)
-      match op with
-      | Add -> Ok (v1 + v2)
-      | Sub -> Ok (v1 - v2)
-      | Mul -> Ok (v1 * v2)
-      | Div -> 
-        if v2 = 0 then Error { error = DivByZero; meta = e2.meta }
-        else Ok (v1 / v2)
-      | Pow -> 
-        if v2 < 0 then Error { error = NegExp; meta = e2.meta }
-        else Ok (int_of_float (float_of_int v1 ** float_of_int v2))
+      (* Evaluate e1 first *)
+      match eval_expr e1 with
+      | Error err -> Error err  (* Immediately return the first encountered error *)
+      | Ok v1 -> (
+          (* Evaluate e2 only if e1 succeeds *)
+          match eval_expr e2 with
+          | Error err -> Error err
+          | Ok v2 -> (
+              match op with
+              | Add -> Ok (v1 + v2)
+              | Sub -> Ok (v1 - v2)
+              | Mul -> Ok (v1 * v2)
+              | Div -> 
+                if v2 = 0 then Error { error = DivByZero; meta = e2.meta }
+                else Ok (v1 / v2)
+              | Pow -> 
+                if v2 < 0 then Error { error = NegExp; meta = e2.meta }
+                else Ok (int_of_float (float_of_int v1 ** float_of_int v2))
+            )
+        )
   in
   eval_expr e
+
 
 exception ListTooShort
 exception InvalidArg
@@ -127,8 +136,6 @@ module DoubleListDequeue : DEQUEUE with type 'a t = 'a list * 'a list = struct
 
   let to_list (front, back) = front @ List.rev back
 end
-
-
 
 module StringMap = Map.Make(String)
 module IntMap = Map.Make(Int)
