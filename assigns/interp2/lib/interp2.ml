f83cae436e60a683f1604f8d4b8c5c062114f21e
r4mcha3 <rchae@bu.edu>
2025-04-09 13:58:33 -0400

    Update interp2.ml

--- assigns/interp2/lib/interp2.ml
+++ assigns/interp2/lib/interp2.ml
@@ -98,18 +98,23 @@ let rec typecheck (env : (string * ty) list) (e : expr) : (ty, error) result =
           | Error err -> Error err)
       | Ok t -> Error (FunAppTyErr t)
       | Error err -> Error err)
-  | Let { is_rec = false; name = x; ty = ty_x; binding = e1; body = e2 } ->
-      (match typecheck env e1 with
-      | Ok t1 -> if t1 = ty_x then typecheck ((x, ty_x) :: env) e2 else Error (LetTyErr (ty_x, t1))
-      | Error err -> Error err)
-  | Let { is_rec = true; name = f; ty = ty_f; binding = e1; body = e2 } ->
-      (match e1 with
-      | Fun (x, ty_x, body) ->
-          let extended = (f, ty_f) :: env in
-          (match typecheck extended e1 with
-          | Ok t1 -> if t1 = ty_f then typecheck extended e2 else Error (LetTyErr (ty_f, t1))
-          | Error err -> Error err)
-      | _ -> Error (LetRecErr f))
+  | Let { is_rec = false; name = x; binding = e1; body = e2; _ } ->
+    let v1 = eval_expr env e1 in
+    eval_expr ((x, v1) :: env) e2
+
+  | Let { is_rec = true; name = f; binding = e1; body = e2; _ } ->
+    match e1 with
+    | Fun (x, _, body) ->
+        let clos_env = List.to_seq env |> Env.of_seq in
+        let rec_clos = VClos {
+          arg = x;
+          body = body;
+          env = clos_env;
+          name = Some f;
+        } in
+        eval_expr ((f, rec_clos) :: env) e2
+    | _ -> failwith "let rec must bind a function"
+
 
 and typecheck_binop env op e1 e2 =
   let err_l expected actual = Error (OpTyErrL (op, expected, actual)) in
